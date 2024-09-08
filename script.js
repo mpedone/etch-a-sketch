67d0e1e444da99bb9478dca4548218f02a16e53d
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-09-08 19:29:42 -0400

    Set container div to display:flex and flex-wrap
    Also set the body width to 900px, as this creates a nice square grid on
    my screen.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const container = document.querySelector(".container");
 
-for(let i=0; i<16; i++) {
+for(let i=0; i<2500; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
     container.appendChild(square);
