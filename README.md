6a950ca3653ac13482258bdc673012cf267b346c
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-09-08 19:22:22 -0400

    Simplify square creation in javascript
    documentCreateFragment is neat, and will probably be good to know
    about, but for this project, a simple for loop seems like it will be
    enough.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # etch-a-sketch
+
 Etch-a-Sketch project from The Odin Project Fundamentals course
--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 const container = document.querySelector(".container");
 
-let docFrag = document.createDocumentFragment();
-
-for(let i = 0; i < 16; i++) {
+for(let i=0; i<16; i++) {
     const square = document.createElement("div");
     square.classList.toggle("square");
-    docFrag.appendChild(square);
-} 
-
-container.appendChild(docFrag); 
+    container.appendChild(square);
+}
