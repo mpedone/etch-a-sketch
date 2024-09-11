0cc13784d968bbaf40e042515c57ed90eb1b6ade
Matthew Pedone <12798189+mpedone@users.noreply.github.com>
2024-09-10 21:32:08 -0400

    Convert the code to build the grid to a function

--- README.md
+++ README.md
@@ -21,3 +21,13 @@ I've reworked the javascript to set the boxes. I found a way to get the dimensio
 ## Fixing the hover
 
 Added code to the for loop for event listeners. I used "mouseenter" to handle the color change when the cursor passes over a square, but I didn't love that it is instantaneous, as it just leads to scribbles, so I used modifier keys to give the user some control. To "draw", hold down the shift key. To erase, hold down the alt/option key. I also added a click listener to give very fine control, but this actually doesn't work as well, I think because the mouseenter listener conflicts a little bit. I think you have to enter the "pixel", and then click for it to work. But, it does turn the "pixel" on AND off, so that's nice. This allowed me to remove the :hover css style.
+
+## The grid size button
+
+I added a button to allow the user to change the size of the grid. For it to work, I need the grid to refresh automatically, which means taking the code I've written and turning it into a function. It als means that the function needs to be called when the page first loads. I used DOMContentLoad at first: 
+
+```javascript
+let numberOfSquares = 16;
+document.addEventListener("DOMContentLoaded", drawingArea(containerWidth));
+```
+And then realized that it wasn't really necessary, and that I could jsut call the drawingArea function directly. Next I will wire up the button to the function.
--- script.js
+++ script.js
@@ -1,26 +1,30 @@
-const container = document.querySelector(".container");
-const styles = document.styleSheets[0];
-let containerWidth = parseInt(document.styleSheets[0].cssRules[4].style["width"]);
-let boxStyles = document.styleSheets[0].cssRules[5].style;
+let numberOfSquares = 16;
 
-let numberOfSquares = 32;
-let boxSize = containerWidth/numberOfSquares;
-boxStyles['width'] = boxStyles['height'] = `${boxSize}px`;
+drawingArea(numberOfSquares)
 
-for(let i=0; i<(numberOfSquares**2); i++) {
-    const square = document.createElement("div");
-    square.classList.toggle("square");
-    container.appendChild(square);
-    square.addEventListener("mouseenter", (e) => {
-        let keys = [];
-        if (e.shiftKey) square.classList.add('purpleSquare');
-        if (e.altKey) square.classList.remove('purpleSquare');
-    square.addEventListener("click", () => {
-        square.classList.toggle('purpleSquare');
-    })
-    });
+function drawingArea(columns) {
+    const container = document.querySelector(".container");
+    const styles = document.styleSheets[0];
+    let containerWidth = parseInt(styles.cssRules[4].style["width"]);
+    let boxStyles = document.styleSheets[0].cssRules[5].style;
+
+    let boxSize = containerWidth/columns;
+    boxStyles['width'] = boxStyles['height'] = `${boxSize}px`;
+
+    for(let i=0; i<(columns**2); i++) {
+        const square = document.createElement("div");
+        square.classList.toggle("square");
+        container.appendChild(square);
+        square.addEventListener("mouseenter", (e) => {
+            let keys = [];
+            if (e.shiftKey) square.classList.add('purpleSquare');
+            if (e.altKey) square.classList.remove('purpleSquare');
+        square.addEventListener("click", () => {
+            square.classList.toggle('purpleSquare');
+        })
+        });
+    }
 }
-
 // const grid_button = document.querySelector("#grid_size");
 // let size;
 // grid_button.addEventListener("click", () => {
